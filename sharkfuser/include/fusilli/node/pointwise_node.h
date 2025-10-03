5ae4ec3da46d881d9b2060a0ddd8c1b44de2fcf8
Ian Wood <ianwood@u.northwestern.edu>
2025-10-03 19:22:41 +0000

    [fusilli] Add asm emitters for pointwise
    Signed-off-by: Ian Wood <ianwood@u.northwestern.edu>

--- sharkfuser/include/fusilli/node/pointwise_node.h
+++ sharkfuser/include/fusilli/node/pointwise_node.h
@@ -33,12 +33,12 @@ public:
       : NodeCRTP(ctx), pointwiseAttr(std::move(attr)) {}
 
   // MLIR assembly emitter helper methods.
-  std::string emitNodePreAsm() const override final { return ""; };
-  std::string getOperandNamesAsm() const override final { return ""; };
-  std::string getOperandTypesAsm() const override final { return ""; };
-  std::string getResultNamesAsm() const override final { return ""; };
-  std::string getResultTypesAsm() const override final { return ""; };
-  std::string getResultNamesAndTypesAsm() const override final { return ""; };
+  std::string emitNodePreAsm() const override final;
+  std::string getOperandNamesAsm() const override final;
+  std::string getOperandTypesAsm() const override final;
+  std::string getResultNamesAsm() const override final;
+  std::string getResultTypesAsm() const override final;
+  std::string getResultNamesAndTypesAsm() const override final;
 
   const std::string &getName() const override final {
     return pointwiseAttr.getName();
